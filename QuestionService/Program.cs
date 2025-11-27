f109fb8f3bbc0b8ee00592cf1714868583dfdc7f
LuckyZhangWeiwei <zhangzhang_10@163.com>
2025-11-27 19:22:48 +0800

    complete section 3-7

--- QuestionService/Program.cs
+++ QuestionService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,6 +19,8 @@ builder.Services.AddAuthentication()
         }
         );
 
+builder.AddNpgsqlDbContext<QuestionDbContext>("questionDb");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,5 +34,19 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapDefaultEndpoints();
+
+using var scope = app.Services.CreateScope(); 
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<QuestionDbContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred seeding the DB.");
+}
+
 
 app.Run();
