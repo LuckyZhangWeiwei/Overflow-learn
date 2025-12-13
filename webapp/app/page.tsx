61d36d5295dc9918275bbf3fef18f279e13a40d2
LuckyZhangWeiwei <zhangzhang_10@163.com>
2025-12-13 14:43:21 +0800

    completed section 7-6

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,15 +1,14 @@
-import {Button} from "@heroui/button";
-import {AcademicCapIcon} from "@heroicons/react/24/solid";
+import { Button } from "@heroui/button";
 
 export default function Home() {
-    return (
-        <div className="flex flex-wrap gap-4 items-center">
-            <Button color="default">Default</Button>
-            <Button color="primary">Primary</Button>
-            <Button color="secondary">Secondary</Button>
-            <Button color="success">Success</Button>
-            <Button color="warning">Warning</Button>
-            <Button color="danger">Danger</Button>
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap gap-4 items-center">
+      <Button color="default">Default</Button>
+      <Button color="primary">Primary</Button>
+      <Button color="secondary">Secondary</Button>
+      <Button color="success">Success</Button>
+      <Button color="warning">Warning</Button>
+      <Button color="danger">Danger</Button>
+    </div>
+  );
 }
--- webapp/components/Providers.tsx
+++ webapp/components/Providers.tsx
@@ -1,10 +1,12 @@
-﻿"use client"
+﻿"use client";
 
-import {HeroUIProvider} from "@heroui/react";
-import {ReactNode} from "react";
+import { HeroUIProvider } from "@heroui/react";
+import { ReactNode } from "react";
 
-export default function Providers({children}: { children: ReactNode }) {
-    return (
-        <HeroUIProvider className={"h-full flex flex-col"}>{children}</HeroUIProvider>
-    );
+export default function Providers({ children }: { children: ReactNode }) {
+  return (
+    <HeroUIProvider className={"h-full flex flex-col"}>
+      {children}
+    </HeroUIProvider>
+  );
 }
